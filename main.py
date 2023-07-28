80f4e11be9f2a299626d3bb1f2e144f3f452bbbe
jeuceda <jeuceda8@gmail.com>
2023-07-27 19:50:45 -0600

    Añadir parte 2

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
+import readchar
+
 nombre_jugador = input('Ingresa el nombre del Jugador: ')
-edad_jugadro = input('Ingrese la edad: ')
-print('Bienvenido al juego, {}. Edad {}'.format(nombre_jugador, edad_jugadro))
+
+print('Bienvenido al juego, {}'.format(nombre_jugador))
+
+while True:
+    keypress = readchar.readkey()
+    if keypress == readchar.key.UP:
+        break
+    print(keypress)
