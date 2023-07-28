97fa975a4b0aeae869fc23395aa330ba029fd052
jeuceda <jeuceda8@gmail.com>
2023-07-27 19:53:27 -0600

    Actualizar Readme

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ nombre_jugador = input('Ingresa el nombre del Jugador: ')
 
 print('Bienvenido al juego, {}'.format(nombre_jugador))
 
+#Parte 2:
+
 while True:
     keypress = readchar.readkey()
     if keypress == readchar.key.UP:
