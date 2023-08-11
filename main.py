71ab0f6a8d2ca9390c3c1302ed7009d7f45b5f8f
jeuceda <jeuceda8@gmail.com>
2023-08-10 19:32:07 -0600

    Orden

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from typing import List, Tuple
 import readchar
 import os
 
@@ -40,7 +39,7 @@ while num <= 50:
     break
 '''
 #parte 4:
-def mapa_a_matriz(mapa_string: str) -> List[List[str]]:
+def mapa_a_matriz(mapa_string: str) -> list[list[str]]:
     #separamos por filas
     filas = mapa_string.strip().split('\n')
     
@@ -51,14 +50,15 @@ def mapa_a_matriz(mapa_string: str) -> List[List[str]]:
     
     return matriz
 
+
 def limpiar_pantalla() -> None:
     os.system("clear")
 
-def mostrar_matriz(matriz: List[List[str]]) -> None:
+def mostrar_matriz(matriz: list[list[str]]) -> None:
     for fila in matriz:
         print(" ".join(fila))
 
-def main(mapa: List[List[str]], posicion_inicial: Tuple[int, int], posicion_final: Tuple[int, int]) -> None:
+def main(mapa: list[list[str]], posicion_inicial: tuple[int, int], posicion_final: tuple[int, int]) -> None:
     px, py = posicion_inicial
     mapa[px][py] = 'P'
 
@@ -68,20 +68,20 @@ def main(mapa: List[List[str]], posicion_inicial: Tuple[int, int], posicion_fina
         tecla = readchar.readkey()
 
         #prev_px, prev_py = px, py
-        new_px, new_py = px, py
+        nuevo_px, nuevo_py = px, py
 
         if tecla == readchar.key.UP:
-            new_px -= 1
+            nuevo_px -= 1
         elif tecla == readchar.key.DOWN:
-            new_px += 1
+            nuevo_px += 1
         elif tecla == readchar.key.LEFT:
-            new_py -= 1
+            nuevo_py -= 1
         elif tecla == readchar.key.RIGHT:
-            new_py += 1
+            nuevo_py += 1
 
-        if 0 <= new_px < len(mapa) and 0 <= new_py < len(mapa[0]) and mapa[new_px][new_py] != '#':
+        if 0 <= nuevo_px < len(mapa) and 0 <= nuevo_py < len(mapa[0]) and mapa[nuevo_px][nuevo_py] != '#':
             mapa[px][py] = '.'
-            px, py = new_px, new_py
+            px, py = nuevo_px, nuevo_py
             mapa[px][py] = 'P'
         else:
             print('posicion invalida', px, py)
@@ -108,6 +108,8 @@ mapa_str = """
 """
 
 mapa = mapa_a_matriz(mapa_str)
+print(mapa)
+mostrar_matriz(mapa)
 #print(mapa)
 main(mapa, (0, 0), (10, 9))
 
