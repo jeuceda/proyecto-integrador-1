c9a0e379e940f77beed80410ef1e351d0989c70f
jeuceda <jeuceda8@gmail.com>
2023-08-21 18:57:40 -0600

    Limpieza del codigo anterior

--- parte5.py
+++ parte5.py
@@ -63,13 +63,13 @@ class JuegoArchivo(Juego):
         path_completo = os.path.join(path_a_mapas, nombre_archivo)
         
         with open(path_completo, 'r') as archivo:
-            # Leer archivo linea por linea
+            
+            # Leer archivo linea por linea, sacamos el mapa
             lineas = archivo.readlines()
             mapa_str = "".join(lineas[1:]).strip()
             print(mapa_str)
-                        
-            print(type(lineas[0]))
             
+            #Extraemos la posicion Inicial y Final del Mapa                        
             posiciones = lineas[0].split()
             
             posicion_inicial = tuple(map(int,posiciones[0:2]))            
@@ -86,93 +86,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
-
-'''    
-def mapa_a_matriz(mapa_string: str) -> list[list[str]]:
-    #separamos por filas
-    filas = mapa_string.strip().split('\n')
-    
-    #Cada fila a lista de carcteres
-    matriz = [list(fila) for fila in filas]
-    matriz[-1].append('#')
-    matriz[-2].append('#')
-    
-    return matriz
-
-
-def limpiar_pantalla() -> None:
-    os.system("clear")
-
-def mostrar_matriz(matriz: list[list[str]]) -> None:
-    for fila in matriz:
-        print(" ".join(fila))
-
-def main(mapa: list[list[str]], posicion_inicial: tuple[int, int], posicion_final: tuple[int, int]) -> None:
-    px, py = posicion_inicial
-    mapa[px][py] = 'P'
-
-    while (px, py) != posicion_final:
-        limpiar_pantalla()
-        mostrar_matriz(mapa)
-        tecla = readchar.readkey()
-
-        #prev_px, prev_py = px, py
-        nuevo_px, nuevo_py = px, py
-
-        if tecla == readchar.key.UP:
-            nuevo_px -= 1
-        elif tecla == readchar.key.DOWN:
-            nuevo_px += 1
-        elif tecla == readchar.key.LEFT:
-            nuevo_py -= 1
-        elif tecla == readchar.key.RIGHT:
-            nuevo_py += 1
-
-        if 0 <= nuevo_px < len(mapa) and 0 <= nuevo_py < len(mapa[0]) and mapa[nuevo_px][nuevo_py] != '#':
-            mapa[px][py] = '.'
-            px, py = nuevo_px, nuevo_py
-            mapa[px][py] = 'P'
-        else:
-            print('posicion invalida', px, py)
-    
-    # Muestra el mapa con el jugador en la posición final.
-    limpiar_pantalla()
-    mostrar_matriz(mapa)
-    print("¡Enhorabuena! Has llegado al final del laberinto.")
-    print(len(mapa), len(mapa[0]))
-    print(px, py)
-
-mapa_str = """
-..###################
-......#...#.......#.#
-#.#####.###.#######.#
-#.............#.#.#.#
-###.#.#.#.###.#.#.#.#
-#...#.#.#...#.......#
-###.###.#.###.#####.#
-#.#.#...#.#.......#.#
-#.#.#.#######.#.#####
-#...#.#...#.#.#.....#
-###.###.#.#.#######.#
-#.....#.#.........#.#
-#####.#######.###.#.#
-#.#...#.#.....#.#...#
-#.#.###.###.###.#.###
-#.....#...#...#...#.#
-###.###.#########.#.#
-#.#.#.#...#...#.....#
-#.#.#.###.###.#.#####
-#.....#.............
-###################.
-"""
-
-mapa = mapa_a_matriz(mapa_str)
-print(mapa)
-mostrar_matriz(mapa)
-#print(mapa)
-main(mapa, (0, 0), (20, 19))
-
-
-'''
